2a09dce7c9dd65b1427f7b375515578c65cb6853
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-05-12 16:06:04 +0100

    Update ecs-task-definition.json.tf

--- terraform.tf/ecs-task-definition.json.tf
+++ terraform.tf/ecs-task-definition.json.tf
@@ -1,19 +1,21 @@
-{
-  "family": "coinbase-app",
-  "networkMode": "awsvpc",
-  "containerDefinitions": [
+resource "aws_ecs_task_definition" "app" {
+  family                   = "react-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
     {
-      "name": "coinbase-app",
-      "image": "851725512876.dkr.ecr.us-west-1.amazonaws.com/coinbase-app:latest",
-      "cpu": 256,
-      "memory": 512,
-      "essential": true,
-      "portMappings": [
+      name      = "react-app",
+      image     = "851725512876.dkr.ecr.us-west-1.amazonaws.com/coinbase-app:latest",
+      portMappings = [
         {
-          "containerPort": 80,
-          "hostPort": 80
+          containerPort = 80
+          hostPort      = 80
         }
       ]
     }
-  ]
+  ])
 }
