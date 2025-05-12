037babb8cebaa5816c81910eb2eff2a74284a37e
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-05-12 16:36:36 +0100

    Update Variables.tf

--- terraform.tf/Variables.tf
+++ terraform.tf/Variables.tf
@@ -9,3 +9,9 @@ variable "subdomain" {
 variable "ecr_repo_name" {
   default = "my-react-app"
 }
+
+variable "app_image" {
+  description = "The Docker image to deploy"
+  type        = string
+  default     = "851725512876.dkr.ecr.us-west-1.amazonaws.com/coinbase-app:latest"
+}
