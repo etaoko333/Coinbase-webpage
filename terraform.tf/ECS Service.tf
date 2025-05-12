573b0a5442b08ef55bacb92c3ffd2337889ad0c3
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-05-12 16:35:53 +0100

    Update ECS Service.tf

--- terraform.tf/ECS Service.tf
+++ terraform.tf/ECS Service.tf
@@ -1,20 +1,17 @@
-resource "aws_ecs_service" "app" {
-  name            = "react-app-service"
-  cluster         = aws_ecs_cluster.main.id
-  launch_type     = "FARGATE"
-  task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = 1
-
-  network_configuration {
-    subnets         = module.vpc.public_subnets
-    assign_public_ip = true
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.app_tg.arn
-    container_name   = "react-app"
-    container_port   = 80
-  }
-  depends_on = [aws_lb_listener.http]
+resource "aws_ecs_task_definition" "app" {
+  family                   = "react-app-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = jsonencode([{
+    name      = "react-app"
+    image     = var.app_image
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
 }
